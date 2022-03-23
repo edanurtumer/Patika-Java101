6e1a5feed8297dd64e44e0260631e2df8156002b
Edanur Tümer <62879275+edanurtumer@users.noreply.github.com>
2022-03-23 21:38:09 +0300

    1-100 Arasındaki Asal Sayıları Bulan Program

---
+++ BirileYüzArasındakiAsalSayilariBulanProgram/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int counter = 0;
+	    for(int number = 2; number <= 100; number++){
+            int control = 0;
+            for(int i = 2; i < number; i++){
+                if(number % i == 0){
+                    control = 1;
+                    break;
+                }
+            }
+            if(control == 0){
+                System.out.print(number + " ");
+                counter++;
+            }
+        }
+    }
+}
